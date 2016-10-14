e7dff053ba1f8dc4c80f8e2451f041e6e38ea1a9
bIropka <biropka@gmail.com>
2016-10-14 17:00:53 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -4,20 +4,40 @@ $(document).ready(function () {
      ******* other scripts ********
      ******************************/
 
+    $('.header-nav > li').hover(
+        function() {
+            if ($(window).width() > '980'){
+                $(this).addClass('active');
+                $(this).find('ul').stop().fadeIn(200);
+            }
+        },
+        function() {
+            if ($(window).width() > '980'){
+                $(this).removeClass('active');
+                $(this).find('ul').stop().fadeOut(200);
+            }
+        }
+    );
+
+    $('.header-nav > li').click(function() {
+        if ($(window).width() < '981'){
+            $(this).toggleClass('active');
+            $(this).find('ul').stop().fadeToggle(200);
+        }
+    });
+
+    $('.burger').click(function() {
+        $(this).toggleClass('active');
+        $(this).siblings('ul').slideToggle();
+    });
+
 
     /******************************
      ******* slider scripts *******
      *****************************/
-    $('.slider-banner').slick({
-        appendArrows: '.slider-banner-controls',
-        prevArrow: '.slider-banner-left',
-        nextArrow: '.slider-banner-right'
-    });
+    $('.slider-banner').slick();
 
-    $('.slider-portfolio').slick({
-        appendArrows: '.slider-portfolio-controls',
-        prevArrow: '.slider-portfolio-left',
-        nextArrow: '.slider-portfolio-right',
+    $('.slider-courses').slick({
         slidesToShow: 4,
         responsive: [
             {
